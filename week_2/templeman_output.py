1689a729a06776b49dbeda64353dfcfc33478ef8
Bernice Templeman <bernice.templeman1@gmail.com>
2024-06-07 20:11:21 -0400

    update to format in templeman_output.py

--- week_2/templeman_output.py
+++ week_2/templeman_output.py
@@ -5,5 +5,5 @@ File Name: templeman_output.py
 Description: This file outputs a quote and gives credit to the originating author.
 """
 # use the print function to output the quote and author.
-print('"My favorite course to teach is {0}'.format('WEB 450 - Mastering the MEAN Stack!"'))
-print('{0}'.format('Professor Krasso'))
+print('"My favorite course to teach is {0}\n{1}'.format(
+    'WEB 450 - Mastering the MEAN Stack!"', 'Professor Krasso'))
