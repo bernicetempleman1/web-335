00b4928223785642d474b884bdb8d3bb5499d8b3
Bernice Templeman <bernice.templeman1@gmail.com>
2024-07-18 16:42:23 -0400

    update commit of week8 whatabook queries

--- whatabook/whatabook-queries.js
+++ whatabook/whatabook-queries.js
@@ -16,3 +16,12 @@ db.books.find({"author": "Arnaud Lauret"})
 
 // Write a query to display a book by bookId.
 db.books.findOne({"bookId": "ISBN-9780321826626"})
+
+// Display wishlist items for a customer
+db.customers.find({ customerId: "c1001" }, { wishlistitems: 1 })
+
+// Add books to a customer’s wishlist: https://www.mongodb.com/docs/manual/reference/operator/update/push/
+db.customers.updateOne({customerId: "c1001"}, {$push: {wishlistitems: {bookId: "ISBN-978-0590353403", genre: "Fantasy", title: "Harry Potter", author: "J.K. Rowling"}}})
+
+// Remove book from a customer’s wishlist: https://www.mongodb.com/docs/manual/reference/operator/update/pull/
+db.customers.updateOne({customerId: "c1001"}, {$pull: {wishlistitems: {bookId: "ISBN-978-0590353403"}}})
